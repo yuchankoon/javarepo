f1c5c82b94faf440ffbd289ffb741543aded6a29
김유찬 <so1no1@naver.com>
2024-04-04 17:43:09 +0900

    feat: create javascript01.js

---
+++ javascript01.js
@@ -0,0 +1,27 @@
+//자바스크립트 변수, 자료형, 함수, 조건문, 반복문
+
+// 변수
+let a = 'a'
+const b = 'b'
+
+let camelCase; 카멜케이스
+let snake_case; 스네이크케이스
+let PascalCase; 파스칼케이스
+let $case1; $ 달러
+let _case2; _ 밑줄
+
+let bread = '빵';
+let creamBread = '크림빵';
+bread = creamBread;
+console.log(bread);
+console.log('첫번째 빵은 : ', bread);
+console.log('두번째 빵은 ; ${bread}');
+
+
+// 자료형
+
+// 함수
+
+// 조건문
+
+// 반복문
